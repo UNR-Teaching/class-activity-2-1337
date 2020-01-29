8b6b96f5831fd4dad1a2c8eb70d48065d84ff485
Ian Grant <igrant@nevada.unr.edu>
2020-01-28 16:08:14 -0800

    init

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,8 @@
-""" Note: Although the skeleton below is in Python, you may use any programming language you want so long as the language supports object-oriented programming, 
+#!/usr/bin/env python3
+""" Note: Although the skeleton below is in Python, you may use any programming language you want so long as the language supports object-oriented programming,
           and you make use of relevant object-oriented design principles.
 """
-~~~~~~~~~~~~
+
 
 class Board(object):
 
@@ -9,6 +10,11 @@ class Board(object):
         """
         Initializes the Board of size 3x3
         """
+        self.board = [['_','_','_'],
+
+                      ['_','_','_'],
+                      ['_','_','_']]
+        self.over = False
 
         pass
 
@@ -22,6 +28,16 @@ class Board(object):
 
         :return: ????
         """
+        player = player.upper()
+        if column > 3 or row > 3:
+            print("Not a valid position!")
+            return 1
+        if player is not "X" or player is not "O":
+            print("Invalid player!")
+            return 1
+
+        self.board[row][column] = player
+        return 0
 
         pass
 
@@ -33,18 +49,24 @@ class Board(object):
         """
 
         pass
+    def print_board(self):
+
 
     def play_game(self):
         """
         Takes moves from raw_input as comma-separated list in form (column, row, player)
             and makes a move. When a winner has been determined, the game ends
-        
+
         :return: (str) the letter representing the player who won
         """
-        
+        while self.over is False:
+            move = input("Input a move(column, row, player): ")
+            moveArr = move.split(',')
+            mark_square(moveArr[0],moveArr[1],moveArr[2])
+
         pass
-        
+
 if __name__ == '__main__':
     board = Board()
     winner = board.play_game()
-    print("{} has won!".format(winner))
+    print(f"{winner} has won!")
